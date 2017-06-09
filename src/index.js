197cdc3c74beaf66dfb7a4c8a3206c550e963f17
Valdeci Gomes <coderade@outlook.com>
2017-06-08 21:08:51 -0300

    Remove trainning code.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import {render} from 'react-dom';
-import {hello, goodbye} from './lib'
-
+import React from 'react'
+import { render } from 'react-dom'
 
 render(
     <div>
-        {hello}
-        {goodbye}
     </div>,
     document.getElementById('react-container')
 );
