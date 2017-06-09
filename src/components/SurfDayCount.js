a04b41a0591b198914688053d8bd0fda0a7b46ab
Valdeci Gomes <coderade@outlook.com>
2017-06-08 22:41:10 -0300

    Add properties to the SurfDayCount, Change to Yarn.
    Update SurfDayCount to use ES6 Syntax.
    Add components properties to the SurfDayCount.
    Change project to use Yarn instead NPM.

--- src/components/SurfDayCount.js
+++ src/components/SurfDayCount.js
@@ -1,23 +1,39 @@
 import React from 'react'
-import createReactClass from 'create-react-class'
 import '../stylesheets/ui.scss'
+import PropTypes from 'prop-types';
 
-
-export const SurfDayCount = createReactClass({
-    displayName: 'SurfDayCount',
+export default class SurfDayCount extends React.Component {
+    constructor() {
+        super();
+    }
+    
     render() {
             return (
                 <div className="surf-day-count">
                     <div className="total-days">
-                        <span>5 days</span>
+                        <span>{this.props.total}</span>
+                        <span>days</span>
                     </div>
                     <div className="powder-days">
-                        <span>2 days</span>
+                        <span>{this.props.powder}</span>
+                        <span>days</span>
                     </div>
                     <div className="backcountry-days">
-                        <span>1 hiking day</span>
+                        <span>{this.props.backcountry}</span>
+                        <span>days</span>
+                    </div>
+                    <div>
+                        <span>{this.props.goal}</span>
                     </div>
                 </div>
             )
     }
-});
+}
+
+SurfDayCount.propTypes = {
+    total: PropTypes.number,
+    powder: PropTypes.number,
+    backcountry: PropTypes.number,
+    goal: PropTypes.number
+};
+
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {SurfDayCount} from './components/SurfDayCount'
+import SurfDayCount from './components/SurfDayCount'
 
 window.React = React;
 
 render(
-    <SurfDayCount />,
+    <SurfDayCount total={50} powder={20} backcountry={10} goal={100}/>,
     document.getElementById('react-container')
 );
