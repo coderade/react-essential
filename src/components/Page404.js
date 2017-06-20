bf739951a21af888fa73b09d793b656d11953c8e
Valdeci Gomes <coderade@outlook.com>
2017-06-19 23:56:17 -0300

    Add Router to the project with React Router library.

---
+++ src/components/Page404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Page404 = () =>
+    <div>
+        <h1>Route not found :(</h1>
+    </div>;
+
+export default Page404;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import {render} from 'react-dom'
 import App from './components/App'
+import Page404 from './components/Page404'
+import {HashRouter, Route} from 'react-router-dom'
+
 
 window.React = React;
 
 render(
-   <App/>,
+    <HashRouter>
+        <div>
+            <Route exact path='/' component={App}/>
+            <Route exact path="*" component={Page404}/>
+        </div>
+    </HashRouter>,
     document.getElementById('react-container')
 );
