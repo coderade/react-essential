e301eaab5105cea8f137dad1994cf4f9f5dd25e2
Valdeci Gomes <coderade@outlook.com>
2017-08-21 22:06:11 -0300

    Update Add Day form component, update main layout.

--- src/components/AddDayForm.js
+++ src/components/AddDayForm.js
@@ -1,7 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 
-const AddDayForm = () =>
-    <h1>Add a Day</h1>;
+class AddDayForm extends Component {
+    render() {
 
+        const {beach, date, hazards, crowded} = this.props;
+
+        return (
+            <form className="add-day-form">
+
+                <label htmlFor="beach">Beach</label>
+                <input id="beach" type="text" defaultValue={beach} required/>
+
+                <label htmlFor="date">Date</label>
+                <input id="date" type="date" defaultValue={date} required/>
+
+                <div>
+                    <input id="hazards" type="checkbox" defaultChecked={hazards} required/>
+                    <label htmlFor="hazards">Dangerous?</label>
+                </div>
+
+                <div>
+                    <input id="crowded" type="checkbox" defaultChecked={crowded} required/>
+                    <label htmlFor="crowded">Is Crowded?</label>
+                </div>
+            </form>
+        )
+    }
+}
+
+AddDayForm.defaultProps = {
+    beach: 'Hawaii',
+    date: '2017-08-29',
+    hazards: true,
+    crowded: true
+
+};
+
+AddDayForm.propTypes = {
+    beach: PropTypes.string.isRequired,
+    date: PropTypes.string.isRequired,
+    hazards: PropTypes.bool.isRequired,
+    crowded: PropTypes.bool.isRequired
+};
 
 export default AddDayForm;
